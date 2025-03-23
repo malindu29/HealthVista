ca9dd2d2a9fd6246859559bfc4d96f5d0cb2a961
malindu29 <malindu1067@gmail.com>
2025-03-23 20:41:32 +0530

    Update API endpoints to use production URL and remove local environment files

--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -10,7 +10,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/message/getall",
+          "https://health-vista.vercel.app/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
